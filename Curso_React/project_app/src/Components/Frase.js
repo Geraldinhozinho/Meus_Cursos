a1d9337a512504ecfb9f002500db93cf80121642
Geraldo Júnior <161983302+Geraldinhozinho@users.noreply.github.com>
2026-04-18 22:03:08 -0300

    Components

---
+++ Curso_React/project_app/src/Components/Frase.js
@@ -0,0 +1,7 @@
+export function Frase(){
+    return(
+        <div>
+            <h1>Minha Primeira Frase</h1>
+        </div>
+    )
+}
---
+++ Curso_React/project_app/src/Components/HelloWorld.js
@@ -0,0 +1,12 @@
+
+import {Frase} from "./components/Frase";
+
+export function HelloWorld(){
+    return(
+        <div>
+            <Frase/>
+            <h1>Hello World</h1>
+            <Frase/>
+        </div>
+    )
+} 
